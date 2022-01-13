93b54339e3891216db9730c4ccf40d845abaa516
farizmaul <ricoayuba88@gmail.com>
2022-01-13 22:27:53 +0700

    Import for whyred
    Signed-off-by: farizmaul <ricoayuba88@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,28 +4,51 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Hyper-X kernel
-do.devicecheck=0
+kernel.string=Hyper-X-Kernel
+dev.string=FarizMaulana
+do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=alioth
-device.name2=aliothin
-supported.versions=
+device.name1=whyred
+supported.versions=9 - 12
+supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/by-name/boot;
-is_slot_device=1;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
+no_block_display=1;
+patch_vbmeta_flag=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-## AnyKernel install
-dump_boot;
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+## AnyKernel boot install
+dump_boot;
 write_boot;
-## end install
+## end boot install
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
